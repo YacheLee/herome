417fc55e84ea0da276ffa3ff8ca56e29aed25813
Scott <mymislife@gmail.com>
2017-04-17 10:02:31 +0800

    git init

---
+++ app/src/main/java/com/dentaltw/herome/Activities/MainActivity.java
@@ -0,0 +1,14 @@
+package com.dentaltw.herome.Activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import com.dentaltw.herome.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
