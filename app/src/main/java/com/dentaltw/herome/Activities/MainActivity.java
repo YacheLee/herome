444adaa0722fe19dc8a48a402f6197da3dc58dcd
Scott <mymislife@gmail.com>
2017-04-19 08:36:56 +0800

    import fragment

--- app/src/main/java/com/dentaltw/herome/Activities/MainActivity.java
+++ app/src/main/java/com/dentaltw/herome/Activities/MainActivity.java
@@ -2,27 +2,43 @@ package com.dentaltw.herome.Activities;
 
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import com.dentaltw.herome.Fragments.MainFragment;
+import com.dentaltw.herome.Fragments.PickPowerFragment;
 import com.dentaltw.herome.R;
 
-public class MainActivity extends AppCompatActivity implements MainFragment.MainFragmentInteractionListener {
+public class MainActivity extends AppCompatActivity implements MainFragment.MainFragmentInteractionListener, PickPowerFragment.PickPowerInteractionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        FragmentManager manager = getSupportFragmentManager();
-//        Fragment fragment = manager.findFragmentById(R.id.fragment_container);
-//        if(fragment!=null){
-//            FragmentTransaction transaction = manager.beginTransaction();
-//            transaction.add(R.id.fragment_container, fragment);
-//            transaction.commit();
-//        }
+        FragmentManager manager = getSupportFragmentManager();
+        Fragment fragment = manager.findFragmentById(R.id.fragment_container);
+
+        if (fragment == null) {
+            fragment = new MainFragment();
+            FragmentTransaction transaction = manager.beginTransaction();
+            transaction.add(R.id.fragment_container, fragment);
+            transaction.commit();
+        }
     }
 
     @Override
     public void onMainFragmentInteraction(Uri uri) {
+
+    }
+
+    public void loadPickPowerScreen(){
+        PickPowerFragment pickPowerFragment = new PickPowerFragment();
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, pickPowerFragment).addToBackStack(null).commit();
+    }
+
+    @Override
+    public void onPickPowerFragmentInteraction(Uri uri) {
 
     }
 }
